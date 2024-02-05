f6b61c6bdc0b2ea60040834d4d6de1e1db0bf932
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-02-05 21:31:32 +0200

    init commit

---
+++ CryptoWallet/CryptoWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoWallet
+//
+//  Created by Vladyslav Lysenko on 05.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoWallet/CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Vladyslav Lysenko on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
