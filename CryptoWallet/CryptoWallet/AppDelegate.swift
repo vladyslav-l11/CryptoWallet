aac9cd838308f25c348967fa23ab9015db048eeb
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-02-06 21:48:03 +0200

    [WIP] project setup

---
+++ CryptoWallet/CryptoWallet/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  CryptoWallet
+//
+//  Created by Vladyslav Lysenko on 05.02.2024.
+//
+
+import UIKit
+import SwiftUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let contentView = ContentView()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+        return true
+    }
+}
