2bd60f33d65f0aec23cfba79d4540d2a62ff874e
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-02-27 22:53:10 +0200

    [WIP] base factory

---
+++ CryptoWallet/CryptoWallet/Views/Base/BaseFactory.swift
@@ -0,0 +1,12 @@
+//
+//  BaseFactory.swift
+//  CryptoWallet
+//
+//  Created by Vladyslav Lysenko on 27.02.2024.
+//
+
+import Foundation
+
+class BaseFactory {
+    let useCases: UseCases = Platform.shared
+}
